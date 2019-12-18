1294e6e651437b40fc4b490e719cbb87db4165ba
Marco <marcopa84@hotmail.it>
2019-12-18 10:18:33 +0100

    exercise DONE!

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var nome = prompt ('inserisci il nome');
+console.log(nome);
+var cognome = prompt ('inserisci il cognome');
+console.log(cognome);
+var colore = prompt ('inserisci il colore');
+console.log(colore);
+var numero = 19;
+
+console.log(nome+cognome+colore+numero);
+
+document.getElementById('password').innerHTML = nome + cognome + colore+ numero
